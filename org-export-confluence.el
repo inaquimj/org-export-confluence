1806d45438909d7cd8a127f0d63e4d086981d701
timedina <timedina@TIMEDINA-LAP.oradev.oraclecorp.com>
2020-08-04 13:09:07 -0500

    Initial working implmentation

--- org-export-confluence.el
+++ org-export-confluence.el
@@ -1,5 +1,11 @@
+(setq org-html-head-include-default-style nil)
+(setq org-html-table-default-attributes nil)
+
 (org-export-define-derived-backend 'confluence-storage 'html
-				   :menu-entry '(?C "Confluence Storage" org-confluence-storage-export-as-confluence-storage))
+				   :menu-entry '(?C "Confluence Storage" org-confluence-storage-export-as-confluence-storage)
+				   :translate-alist '((template . org-cs-exp-template)
+						      (inner-template . org-cs-exp-inner-template)))
+
 
  ;;;###autoload
 (defun org-confluence-storage-export-as-confluence-storage
@@ -7,3 +13,6 @@
   (interactive)
   (org-export-to-buffer 'confluence-storage "*Org Confluence Storage Export*"
     async subtreep visible-only body-only ext-plist (lambda () (html-mode))))   
+
+(defun org-cs-exp-template (contents info) contents)
+(defun org-cs-exp-inner-template (contents info) (concat "<ac:structured-macro ac:macro-id=\"954618d1-3832-4140-a7c1-e0120ef6d3dc\" ac:name=\"toc\" ac:schema-version=\"1\"/>\n\n" contents))
