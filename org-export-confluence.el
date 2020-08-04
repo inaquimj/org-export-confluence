de5853e331e21549acffb60643a04699703c3a68
Perimetro20 <a01205778@itesm.mx>
2020-08-03 21:25:33 -0500

    Implement basic interface for use of the export module. Extend the README

--- org-export-confluence.el
+++ org-export-confluence.el
@@ -1,5 +1,9 @@
-(defun org-export-confluence
+(org-export-define-derived-backend 'confluence-storage 'html
+				   :menu-entry '(?C "Confluence Storage" org-confluence-storage-export-as-confluence-storage))
+
+ ;;;###autoload
+(defun org-confluence-storage-export-as-confluence-storage
 (&optional async subtreep visible-only body-only ext-plist)
   (interactive)
-  (org-export-to-buffer 'latex "*Org LATEX Export*"
-    async subtreep visible-only body-only ext-plist (lambda () (LaTeX-mode))))
+  (org-export-to-buffer 'confluence-storage "*Org Confluence Storage Export*"
+    async subtreep visible-only body-only ext-plist (lambda () (html-mode))))   
